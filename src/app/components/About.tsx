cf71b2dc57c03233582597bea03bdba07f8027e5
surafelgk <118168857+Surafelgk@users.noreply.github.com>
2026-01-08 12:09:23 +0300

    NEW ADD HEAD

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -49,8 +49,8 @@ export default function About() {
 
       {/* Education */}
       <p className="text-sm mt-2 text-muted"> 
-       - B.S. Computer Science graduate from <b> Unity University </b> with a strong self taught background in <b>Dgital Marketing Specialist</b> and{" "}
-        <b>video editor/ Graphics Designer</b> 
+       - B.S. Computer Science graduate from <b> Unity University </b> With a strong self taught background in <b>Dgital Marketing Specialist</b> and{" "}
+        <b>Video editor/ Graphics Designer</b> 
       </p>
 
       {/* Involvement */}
